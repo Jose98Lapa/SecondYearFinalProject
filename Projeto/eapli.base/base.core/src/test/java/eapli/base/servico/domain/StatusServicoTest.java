9ce947f721c2773bfdf55491ec9e1bcfdb84fc76
1190731 <1190731@isep.ipp.pt>
2021-05-07 20:46:57 +0100

    LAPR4-11 Fix DB conflict

--- Projeto/eapli.base/base.core/src/test/java/eapli/base/servico/domain/StatusServicoTest.java
+++ Projeto/eapli.base/base.core/src/test/java/eapli/base/servico/domain/StatusServicoTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StatusServicoTest {
-    StatusServico StatusServico = new StatusServico("ACTIVE");
+    StatusServico StatusServico = new StatusServico("ATIVO");
 
     @Test
     public void test() {
-        assertEquals(StatusServico, new StatusServico("ACTIVE"));
-        assertNotEquals(StatusServico, new StatusServico("INACTIVE"));
+        assertEquals(StatusServico, new StatusServico("ATIVO"));
+        assertNotEquals(StatusServico, new StatusServico("INATIVO"));
         assertThrows(IllegalArgumentException.class,() -> new StatusServico("A22"));
         assertThrows(IllegalArgumentException.class,() -> new StatusServico(" "));
         assertThrows(IllegalArgumentException.class,() -> new StatusServico(null));
