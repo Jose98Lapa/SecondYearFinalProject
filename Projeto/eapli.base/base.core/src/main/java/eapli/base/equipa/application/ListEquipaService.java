4ee061ee2b39d09f2a0bedcff9e7f07d701681fe
Tiago Silva Machado <1191097@isep.ipp.pt>
2021-05-08 02:10:42 +0100

    LAPR4-10 UC2001 - Implementação - algumas mudanças

--- Projeto/eapli.base/base.core/src/main/java/eapli/base/equipa/application/ListEquipaService.java
+++ Projeto/eapli.base/base.core/src/main/java/eapli/base/equipa/application/ListEquipaService.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class ListEquipaService {
     private final EquipaRepository teamRepo = PersistenceContext.repositories().teams();
 
-    public Iterable<EquipaDTO> teams(){
+    public Iterable<EquipaDTO> allTeams(){
         Set<EquipaDTO> equipas = new HashSet<>();
         teamRepo.findAll().forEach(equipa -> equipas.add(equipa.toDTO()));
         return equipas;
