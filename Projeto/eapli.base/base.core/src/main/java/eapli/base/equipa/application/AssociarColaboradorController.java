133bdcd0966fc27954f499c7be7f67765d828910
José Lapa <joselapa@Joses-MBP.lan>
2021-05-09 22:11:18 +0100

    UC9001 - Implementation - apresentacao

--- Projeto/eapli.base/base.core/src/main/java/eapli/base/equipa/application/AssociarColaboradorController.java
+++ Projeto/eapli.base/base.core/src/main/java/eapli/base/equipa/application/AssociarColaboradorController.java
@@ -8,6 +8,9 @@ import eapli.base.equipa.domain.Equipa;
 import eapli.base.equipa.repositories.EquipaRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AssociarColaboradorController {
 
     private final EquipaRepository equipaRepository = PersistenceContext.repositories().teams();
@@ -21,4 +24,28 @@ public class AssociarColaboradorController {
         ListColaboradorService servicoListarColaboradores = new ListColaboradorService();
         return servicoListarColaboradores.colaboradores();
     }
+
+    public void save( EquipaDTO equipaDTO ) {
+        Colaborador colaborador = new ColaboradorDTOParser().valueOf(equipaDTO.colaboradorDTO);
+        TipoEquipa tipoEquipa = new TipoEquipaDTOParser().valueOf(equipaDTO.tipoEquipaDTO);
+
+        if (equipaRepository.canIAddTheColaborador(colaborador, tipoEquipa)) {
+            List<Colaborador> colaboradorList = new ArrayList<>();
+            colaboradorList.add(colaborador);
+
+            Equipa equipa = equipaBuilder
+                    .designacao(equipaDTO.descricao)
+                    .acronimo(equipaDTO.acronimo)
+                    .equipaID(equipaDTO.equipaID)
+                    .responsaveis(colaboradorList)
+                    .tipoDeEquipa(tipoEquipa)
+                    .build();
+
+            equipaRepository.save(equipa);
+
+        } else {
+            throw new IllegalArgumentException("Este colaborador já gere uma equipa deste tipo");
+        }
+    }
+
 }
