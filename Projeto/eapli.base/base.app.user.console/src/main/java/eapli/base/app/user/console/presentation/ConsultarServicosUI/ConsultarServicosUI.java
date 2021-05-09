12fb6af53659639a514a440166aaefb4bdb23fc6
= <1190641@isep.ipp.pt>
2021-05-09 11:34:33 +0100

    LAPR4-14 implementacao da funcionalidade de procura por diferentes parametros relativo à UC 3001 - Consultar catálogos de serviços e respetivos serviços que me estão disponibilizados

--- Projeto/eapli.base/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/ConsultarServicosUI/ConsultarServicosUI.java
+++ Projeto/eapli.base/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/ConsultarServicosUI/ConsultarServicosUI.java
@@ -2,6 +2,7 @@ package eapli.base.app.user.console.presentation.ConsultarServicosUI;
 
 import eapli.base.catalogo.application.ConsultarServicosController;
 import eapli.base.servico.DTO.ServicoDTO;
+import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
 
 import java.util.List;
@@ -10,12 +11,21 @@ public class ConsultarServicosUI extends AbstractUI {
     ConsultarServicosController consultarServicosController = new ConsultarServicosController();
     @Override
     protected boolean doShow() {
-        int tipo = 0;
+        int tipo = Console.readInteger("Insira 1 para o titulo\n Insira 2 para o ID\n Insira 3 para procurar por status\n Insira 4 para mostrar todos");
+
         String valor = "";
+        if (tipo!=4){
+            valor = Console.readLine("Insira o valor");
+        }
 
         List<ServicoDTO> servicoDTOList = consultarServicosController.procuraServico(tipo,valor);
         for (ServicoDTO servicoDTO:servicoDTOList){
-            System.out.println(servicoDTO);
+            if (servicoDTO.status.equals("INCOMPLETO")){
+                System.out.printf("O servico com o ID %s estará brevemente disponivel\n",servicoDTO.id);
+            }else if (servicoDTO.status.equals("ATIVO")){
+                System.out.println(servicoDTO);
+            }
+
         }
 
         return true;
