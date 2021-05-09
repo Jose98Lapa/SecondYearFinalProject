e989ebb0ba2df97474c0e09255fc2f23c866c6a6
Tiago Silva Machado <1191097@isep.ipp.pt>
2021-05-09 17:12:09 +0100

    LAPR4-10 UC 2001 - Implementação - simplificação

--- Projeto/eapli.base/base.core/src/main/java/eapli/base/catalogo/dto/CatalogoDTORepresentationBuilder.java
+++ Projeto/eapli.base/base.core/src/main/java/eapli/base/catalogo/dto/CatalogoDTORepresentationBuilder.java
@@ -1,27 +1,4 @@
 /*
- * Copyright (c) 2013-2020 the original author or authors.
- *
- * MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *//*
-
 package eapli.base.catalogo.dto;
 
 import eapli.base.catalogo.domain.Status;
@@ -33,14 +10,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Set;
 
-*/
-/**
- * The builder for a CATALOGDTO using the RepresentationBuilder interface. This builder is forgiving in the sense that it
- * ignores properties that it doesn't understand in case the CATALOG has evolved to support new properties.
- *
- * @author Paulo Gandra de Sousa
- *
- *//*
+
 
 public class CatalogoDTORepresentationBuilder implements RepresentationBuilder<CatalogoDTO> {
 
