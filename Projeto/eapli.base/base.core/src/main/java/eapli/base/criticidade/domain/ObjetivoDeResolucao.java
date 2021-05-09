ba716601a1cbd09ab0936b8845c8ba251db0ac02
1191111 <1191111@isep.ipp.pt>
2021-05-09 01:50:28 +0100

    LAPR4-18 UC2010 - Implementation - Domain value object and entity

--- Projeto/eapli.base/base.core/src/main/java/eapli/base/criticidade/domain/ObjetivoDeResolucao.java
+++ Projeto/eapli.base/base.core/src/main/java/eapli/base/criticidade/domain/ObjetivoDeResolucao.java
@@ -1,4 +1,12 @@
 package eapli.base.criticidade.domain;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class ObjetivoDeResolucao extends ObjetivoSLA{
+    public ObjetivoDeResolucao(String tempMaxA, String tempMedA) {
+        super(tempMaxA,tempMedA);
+    }
+
+    protected ObjetivoDeResolucao() {}
 }
