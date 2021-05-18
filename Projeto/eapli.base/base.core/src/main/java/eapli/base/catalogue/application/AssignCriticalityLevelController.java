4092a621922a8708e6190f8d7e60be8197d34fab
Tiago Silva Machado <1191097@isep.ipp.pt>
2021-05-18 01:39:12 +0100

    LAPR4-47 - US2012 - implementação - controller e UI implementados

---
+++ Projeto/eapli.base/base.core/src/main/java/eapli/base/catalogue/application/AssignCriticalityLevelController.java
@@ -0,0 +1,40 @@
+package eapli.base.catalogue.application;
+
+import eapli.base.catalogue.domain.Catalogue;
+import eapli.base.catalogue.dto.CatalogueDTO;
+import eapli.base.catalogue.dto.CatalogueDTOParser;
+import eapli.base.catalogue.repositories.CatalogueRepository;
+import eapli.base.criticality.application.CriticalityListService;
+import eapli.base.criticality.dto.CriticalityDTO;
+import eapli.base.criticality.dto.CriticalityDTOParser;
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+
+public class AssignCriticalityLevelController {
+
+    private final AuthorizationService authz = AuthzRegistry.authorizationService();
+    private final ListCatalogueService listCatalogueSvc = new ListCatalogueService();
+    private final CriticalityListService listCriticidadesSvc = new CriticalityListService();
+    private final CatalogueRepository catalogRepo = PersistenceContext.repositories().catalogs();
+
+    public Iterable<CatalogueDTO> getCatalogueLst(){
+        return listCatalogueSvc.allCatalogos();
+    }
+
+    public Iterable<CriticalityDTO> getCriticalityLst(){
+        return listCriticidadesSvc.criticidades();
+    }
+
+    public CatalogueDTO changeCriticalityLevel(CatalogueDTO catDto, CriticalityDTO critDto){
+        Catalogue temp = new CatalogueDTOParser().valueOf(catDto);
+        temp.changeCriticalityLevelTo(new CriticalityDTOParser().valueOf(critDto));
+        return temp.toDTO();
+    }
+
+    public void saveCatalog(CatalogueDTO dto){
+        catalogRepo.save(new CatalogueDTOParser().valueOf(dto));
+    }
+
+
+}
