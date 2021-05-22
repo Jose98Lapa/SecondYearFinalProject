4e1e9e4cf469c391e28817502947fc49cec55491
= <1190641@isep.ipp.pt>
2021-05-22 15:21:37 +0100

    LAPR4-36 correção na UC1006 - Desenvolver a componente representativa de uma tarefa automatica

--- Projeto/eapli.base/base.core/src/main/java/eapli/base/specifiedTask/DTO/SpecifiedManualTaskDTOParser.java
+++ Projeto/eapli.base/base.core/src/main/java/eapli/base/specifiedTask/DTO/SpecifiedManualTaskDTOParser.java
@@ -3,12 +3,12 @@ package eapli.base.specifiedTask.DTO;
 import eapli.base.form.DTO.FormDTOParser;
 import eapli.base.specifiedTask.domain.SpecifiedManualSpecifiedTask;
 import eapli.base.specifiedTask.domain.TaskID;
-import eapli.base.specifiedTask.domain.TaskStatus;
+
 import eapli.framework.representations.dto.DTOParser;
 
 public class SpecifiedManualTaskDTOParser implements DTOParser<SpecifiedManualTaskDTO, SpecifiedManualSpecifiedTask> {
     @Override
     public SpecifiedManualSpecifiedTask valueOf(SpecifiedManualTaskDTO dto) {
-        return new SpecifiedManualSpecifiedTask(TaskID.valueOf(dto.taskID), TaskStatus.valueOf(dto.taskStatus), new FormDTOParser().valueOf(dto.formDTO));
+        return new SpecifiedManualSpecifiedTask(TaskID.valueOf(dto.taskID), new FormDTOParser().valueOf(dto.formDTO));
     }
 }
