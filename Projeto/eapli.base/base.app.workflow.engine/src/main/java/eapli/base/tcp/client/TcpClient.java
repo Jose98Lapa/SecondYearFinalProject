bb9ef3d4e5e8a21fe5c41d44154964bc9c10d864
José Lapa <1200619@isep.ipp.pt>
2021-05-22 20:00:45 +0100

    LAPR4-55 Added TCP Client/Server

---
+++ Projeto/eapli.base/base.app.workflow.engine/src/main/java/eapli/base/tcp/client/TcpClient.java
@@ -0,0 +1,83 @@
+package eapli.base.tcp.client;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+class TcpClient {
+
+	static InetAddress serverIP;
+	static Socket sock;
+
+	public static void main ( String[] args ) throws Exception {
+		if ( args.length != 1 ) {
+			System.out.println( "Server IPv4/IPv6 address or DNS name is required as argument" );
+			System.exit( 1 );
+		}
+
+		try {
+			serverIP = InetAddress.getByName( args[0] );
+		} catch ( UnknownHostException ex ) {
+			System.out.println( "Invalid server specified: " + args[0] );
+			System.exit( 1 );
+		}
+
+		try {
+			sock = new Socket( serverIP, 9999 );
+		} catch ( IOException ex ) {
+			System.out.println( "Failed to establish TCP connection" );
+			System.exit( 1 );
+		}
+
+		BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+		DataOutputStream sOut = new DataOutputStream( sock.getOutputStream( ) );
+		DataInputStream sIn = new DataInputStream( sock.getInputStream( ) );
+
+
+		String frase;
+		long f, i, n, num;
+		do {
+			num = onMessage( in, sOut, sIn );
+		}
+		while ( num != 0 );
+		sock.close( );
+	}
+
+	private static long onMessage ( BufferedReader in, DataOutputStream sOut, DataInputStream sIn ) throws IOException {
+		String frase;
+		long i;
+		long num;
+		long f;
+		long n;
+		do {
+			num = -1;
+			while ( num < 0 ) {
+				System.out.print( "Enter a positive integer to SUM (zero to terminate): " );
+				frase = in.readLine( );
+				try {
+					num = Integer.parseInt( frase );
+				} catch ( NumberFormatException ex ) {
+					num = -1;
+				}
+				if ( num < 0 ) System.out.println( "Invalid number" );
+			}
+			n = num;
+			for ( i = 0; i < 4; i++ ) {
+				sOut.write( ( byte ) ( n % 256 ) );
+				n = n / 256;
+			}
+		}
+		while ( num != 0 );
+		num = 0;
+		f = 1;
+		for ( i = 0; i < 4; i++ ) {
+			num = num + f * sIn.read( );
+			f = f * 256;
+		}
+		System.out.println( "SUM RESULT = " + num );
+		return num;
+	}
+
+}
+
