65671ac2e98608f255a9ecf64ebf8640eb975098
1190731 <1190731@isep.ipp.pt>
2021-05-23 09:42:06 +0100

    LAPR4-62 added multiple file type restrictions and link casting for easier implementation.

--- Projeto/eapli.base/base.app.file.server/src/main/java/SFTPClient.java
+++ Projeto/eapli.base/base.app.file.server/src/main/java/SFTPClient.java
@@ -1,6 +1,8 @@
 import com.jcraft.jsch.*;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.image.ImageFilter;
 import java.io.File;
 
 public class SFTPClient {
@@ -16,53 +18,87 @@ public class SFTPClient {
     public void connect() throws JSchException {
         JSch jsch = new JSch();
 
-         session = jsch.getSession("root", host, port);
-         session.setPassword("123Tiago123");
+        session = jsch.getSession("root", host, port);
+        session.setPassword("123Tiago123");
 
         session.setConfig("StrictHostKeyChecking", "no");
         session.connect();
     }
-    public void upload(String source,String filename) throws JSchException, SftpException {
-        filename+=".";
+
+    public void upload(String source, String filename) throws JSchException, SftpException {
+        filename += ".";
         int i = source.lastIndexOf('.');
         if (i > 0) {
-            filename += source.substring(i+1);
+            filename += source.substring(i + 1);
         }
         Channel channel = session.openChannel("sftp");
         channel.connect();
         ChannelSftp sftpChannel = (ChannelSftp) channel;
         try {
-            sftpChannel.cd( "/var/www/html" );
-        }
-        catch ( SftpException e ) {
-            sftpChannel.mkdir( "/var/www/html" );
-            sftpChannel.cd( "/var/www/html" );
+            sftpChannel.cd("/var/www/html");
+        } catch (SftpException e) {
+            sftpChannel.mkdir("/var/www/html");
+            sftpChannel.cd("/var/www/html");
         }
         sftpChannel.put(source, filename);
         sftpChannel.exit();
         session.disconnect();
     }
+
     public void download(String source, String destination) throws JSchException, SftpException {
         Channel channel = session.openChannel("sftp");
         channel.connect();
         ChannelSftp sftpChannel = (ChannelSftp) channel;
         try {
-            sftpChannel.cd( "/var/www/html" );
-        }
-        catch ( SftpException e ) {
-            sftpChannel.mkdir( "/var/www/html" );
-            sftpChannel.cd( "/var/www/html" );
+            sftpChannel.cd("/var/www/html");
+        } catch (SftpException e) {
+            sftpChannel.mkdir("/var/www/html");
+            sftpChannel.cd("/var/www/html");
         }
         sftpChannel.get(source, destination);
         sftpChannel.exit();
         session.disconnect();
     }
-    public void chooseFile(){
+
+    public String chooser(String type) {
         JFileChooser chooser = new JFileChooser();
+        switch (type) {
+            case "image": {
+                chooser.addChoosableFileFilter(new FileNameExtensionFilter("Image Files", "jpg"));
+                chooser.setAcceptAllFileFilterUsed(false);
+            }
+            case "file": {
+                chooser.addChoosableFileFilter(new FileNameExtensionFilter("Image Files", "png"));
+                chooser.setAcceptAllFileFilterUsed(false);
+            }
+            case "script": {
+                chooser.addChoosableFileFilter(new FileNameExtensionFilter("Iage Files", "jpg", "png"));
+                chooser.setAcceptAllFileFilterUsed(false);
+            }
+        }
         int result = chooser.showDialog(null, "Select File");
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = chooser.getSelectedFile();
-            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+            return selectedFile.getAbsolutePath();
+        }
+        return null;
+    }
+
+    public String castLink(String filename) {
+        return "http://"+host+"/"+filename+".jpg";
+    }
+
+    public String choseAndUploadImage(String filename) {
+        try {
+            this.connect();
+            String path = this.chooser("image");
+            if (path != null) {
+                this.upload(path, filename);
+            }
+            return this.castLink(filename);
+        } catch (JSchException | SftpException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
--- Projeto/eapli.base/base.app.file.server/src/main/java/main.java
+++ Projeto/eapli.base/base.app.file.server/src/main/java/main.java
@@ -4,8 +4,6 @@ import com.jcraft.jsch.SftpException;
 public class main {
     public static void main(String[] args) throws JSchException, SftpException {
         SFTPClient server = new SFTPClient();
-        server.chooseFile();
-        server.connect();
-        server.upload("C:/Users/jpcg8/Desktop/ended.png","image");
+        System.out.println(server.choseAndUploadImage("image2"));
     }
 }
