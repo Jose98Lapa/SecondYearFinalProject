797d61db062dec7f46bf595bcc34413a4940ce44
= <1190641@isep.ipp.pt>
2021-05-28 13:35:00 +0100

    LAPR4-35 correcao do commit anterior na UC1005 - Desenvolver a componente representativa de uma tarefa manual

--- Projeto/eapli.base/base.core/src/main/java/eapli/base/task/domain/Task.java
+++ Projeto/eapli.base/base.core/src/main/java/eapli/base/task/domain/Task.java
@@ -10,6 +10,10 @@ import java.io.Serializable;
 public abstract class Task implements DomainEntity<TaskID>, AggregateRoot<TaskID> , Serializable {
     @EmbeddedId
     protected TaskID taskID;
+    @OneToOne
+    private Task beforeTask;
+    @OneToOne
+    private Task afterTask;
 
 
 
@@ -22,4 +26,29 @@ public abstract class Task implements DomainEntity<TaskID>, AggregateRoot<TaskID
     protected Task() {
         // For ORM
     }
+
+    public void editBeforeTask(Task beforeTask){
+        if (this.getClass()==AutomaticTask.class||this.getClass()==ExecutionTask.class){
+            if (beforeTask.getClass()!=ApprovalTask.class){
+                throw new IllegalArgumentException("You can't have an execution task before an execution task");
+            }else{
+                this.beforeTask= beforeTask;
+            }
+        }
+
+
+
+    }
+
+    public void editAfterTask(Task afterTask){
+        if (this.getClass()==ApprovalTask.class){
+            if (afterTask.getClass()==ApprovalTask.class){
+                throw new IllegalArgumentException("You can't have an approval task after an approval task");
+            }else{
+                this.afterTask= afterTask;
+            }
+        }
+
+    }
+
 }
