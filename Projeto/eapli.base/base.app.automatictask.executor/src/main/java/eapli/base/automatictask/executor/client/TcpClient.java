9693bd13f2a66f21f16a85ba8e7b3ebea8195a6e
1191111 <1191111@isep.ipp.pt>
2021-05-28 02:58:43 +0100

    LAPR4-43 - Implementation - TcpClient

--- Projeto/eapli.base/base.app.automatictask.executor/src/main/java/eapli/base/automatictask/executor/client/TcpClient.java
+++ Projeto/eapli.base/base.app.automatictask.executor/src/main/java/eapli/base/automatictask/executor/client/TcpClient.java
@@ -5,10 +5,10 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-class TcpClient {
+public class TcpClient {
 
-	static InetAddress serverIP;
-	static Socket sock;
+	private static InetAddress serverIP;
+	private static Socket socket;
 
 	public static void main ( String[] args ) throws Exception {
 		if ( args.length != 1 ) {
@@ -16,68 +16,32 @@ class TcpClient {
 			System.exit( 1 );
 		}
 
+		TcpClient tcpClient = new TcpClient();
+		tcpClient.startConnection(args[0]);
+
+	}
+
+	private void startConnection(String ip) throws IOException {
 		try {
-			serverIP = InetAddress.getByName( args[0] );
+			serverIP = InetAddress.getByName(ip);
 		} catch ( UnknownHostException ex ) {
-			System.out.println( "Invalid server specified: " + args[0] );
+			System.out.println( "Invalid server specified: " + ip );
 			System.exit( 1 );
 		}
 
 		try {
-			sock = new Socket( serverIP, 9999 );
+			socket = new Socket( serverIP, 9999 );
 		} catch ( IOException ex ) {
 			System.out.println( "Failed to establish TCP connection" );
 			System.exit( 1 );
 		}
 
 		BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
-		DataOutputStream sOut = new DataOutputStream( sock.getOutputStream( ) );
-		DataInputStream sIn = new DataInputStream( sock.getInputStream( ) );
-
-
-		String frase;
-		long f, i, n, num;
-		do {
-			num = onMessage( in, sOut, sIn );
-		}
-		while ( num != 0 );
-		sock.close( );
+		DataOutputStream sOut = new DataOutputStream( socket.getOutputStream( ) );
+		DataInputStream sIn = new DataInputStream( socket.getInputStream( ) );
 	}
 
-	private static long onMessage ( BufferedReader in, DataOutputStream sOut, DataInputStream sIn ) throws IOException {
-		String frase;
-		long i;
-		long num;
-		long f;
-		long n;
-		do {
-			num = -1;
-			while ( num < 0 ) {
-				System.out.print( "Enter a positive integer to SUM (zero to terminate): " );
-				frase = in.readLine( );
-				try {
-					num = Integer.parseInt( frase );
-				} catch ( NumberFormatException ex ) {
-					num = -1;
-				}
-				if ( num < 0 ) System.out.println( "Invalid number" );
-			}
-			n = num;
-			for ( i = 0; i < 4; i++ ) {
-				sOut.write( ( byte ) ( n % 256 ) );
-				n = n / 256;
-			}
-		}
-		while ( num != 0 );
-		num = 0;
-		f = 1;
-		for ( i = 0; i < 4; i++ ) {
-			num = num + f * sIn.read( );
-			f = f * 256;
-		}
-		System.out.println( "SUM RESULT = " + num );
-		return num;
-	}
+	private void serviceList(){
 
+	}
 }
-
