f3afb488404bec62d3f69af7f282099b4c11074c
José Lapa <1200619@isep.ipp.pt>
2021-05-29 18:13:57 +0100

    LAPR4-38 Workflow fix

--- Projeto/eapli.base/base.core/src/main/java/eapli/base/ticketTask/domain/TicketApprovalTask.java
+++ Projeto/eapli.base/base.core/src/main/java/eapli/base/ticketTask/domain/TicketApprovalTask.java
@@ -1,14 +1,19 @@
 package eapli.base.ticketTask.domain;
 
+import eapli.base.collaborator.domain.Collaborator;
 import eapli.base.form.domain.Form;
 import eapli.base.ticketTask.DTO.TicketApprovalTaskDTO;
 import eapli.framework.representations.dto.DTOable;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import java.io.Serializable;
 
 @Entity
 public class TicketApprovalTask extends TicketManualTask implements DTOable< TicketApprovalTaskDTO >, Serializable {
+
+	@OneToOne
+	private Collaborator approvedBy;
 
 	protected TicketApprovalTask () {
 
@@ -28,7 +33,8 @@ public class TicketApprovalTask extends TicketManualTask implements DTOable< Tic
 				this.ticketTaskID.toString(),
 				this.transition().previousTask().toString(),
 				this.transition().nextTask().toString(),
-				this.form()
+				this.form(),
+				approvedBy
 		);
 	}
 
