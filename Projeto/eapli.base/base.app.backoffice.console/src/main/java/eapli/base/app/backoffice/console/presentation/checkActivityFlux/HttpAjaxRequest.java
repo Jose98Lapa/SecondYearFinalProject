c06b03bff3f7068a7e048a41f178a2e72ffa8292
= <1190641@isep.ipp.pt>
2021-05-29 19:20:40 +0100

    LAPR4-46 implementacao inicial US2011 - Conhecer o estado atual do motor de fluxos de atividades

---
+++ Projeto/eapli.base/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/checkActivityFlux/HttpAjaxRequest.java
@@ -0,0 +1,65 @@
+package eapli.base.app.backoffice.console.presentation.checkActivityFlux;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * @author ANDRE MOREIRA (asc@isep.ipp.pt)
+ */
+public class HttpAjaxRequest extends Thread {
+    String baseFolder;
+    Socket sock;
+    DataInputStream inS;
+    DataOutputStream outS;
+
+    public HttpAjaxRequest(Socket s, String f) {
+        baseFolder = f;
+        sock = s;
+    }
+
+
+    public void run() {
+        try {
+            outS = new DataOutputStream(sock.getOutputStream());
+            inS = new DataInputStream(sock.getInputStream());
+        } catch (IOException ex) {
+            System.out.println("Thread error on data streams creation");
+        }
+        try {
+            HTTPmessage request = new HTTPmessage(inS);
+            HTTPmessage response = new HTTPmessage();
+            //System.out.println(request.getURI());
+
+            if (request.getMethod().equals("GET")) {
+                if (request.getURI().equals("/refreshStatus")) {
+                    response.setContentFromString(HttpServerAjax.getStatusInfoStandingInHTML(), "text/html");
+                    response.setResponseStatus("200 Ok");
+                } else {
+                    String fullname = baseFolder + "/";
+                    if (request.getURI().equals("/")) {
+                        fullname = fullname + "index.html";
+                    } else {
+                        fullname = fullname + request.getURI();
+                    }
+                    if (response.setContentFromFile(fullname)) {
+                        response.setResponseStatus("200 Ok");
+                    } else {
+                        response.setContentFromString(
+                                "<html><body><h1>404 File not found</h1></body></html>",
+                                "text/html");
+                        response.setResponseStatus("404 Not Found");
+                    }
+                }
+                response.send(outS);
+            }
+        } catch (IOException ex) {
+            System.out.println("Thread error when reading request");
+        }
+        try {
+            sock.close();
+        } catch (IOException ex) {
+            System.out.println("CLOSE IOException");
+        }
+    }
+}
+
