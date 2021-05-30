e3ec1394834729d901aba85314a9b1eef559a7d4
Tiago Silva Machado <1191097@isep.ipp.pt>
2021-05-30 13:27:37 +0100

    LAPR4-42 - US4001 - Implementação - JpaTicketTaskRepository implementação do metodo getTicketsByCollaborator

--- Projeto/eapli.base/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaTicketTaskRepository.java
+++ Projeto/eapli.base/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaTicketTaskRepository.java
@@ -1,11 +1,14 @@
 package eapli.base.persistence.impl.jpa;
 
 import eapli.base.Application;
+import eapli.base.collaborator.domain.Collaborator;
 import eapli.base.ticketTask.domain.TicketTask;
 import eapli.base.ticketTask.domain.TicketTaskID;
 import eapli.base.ticketTask.repository.TicketTaskRepository;
 import eapli.framework.domain.repositories.TransactionalContext;
 import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
+
+import java.util.List;
 
 public class JpaTicketTaskRepository extends JpaAutoTxRepository< TicketTask, TicketTaskID, TicketTaskID > implements TicketTaskRepository {
 
@@ -16,5 +19,10 @@ public class JpaTicketTaskRepository extends JpaAutoTxRepository< TicketTask, Ti
 	public JpaTicketTaskRepository ( String puname ) {
 		super(puname, Application.settings().getExtendedPersistenceProperties(),
 				"TicketTaskID");
+	}
+
+	@Override
+	public List<TicketTask> getTicketsByCollaborator(Collaborator collab) {
+		return null;
 	}
 }
