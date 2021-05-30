a017195d15c55879273f58f0b0c3786e4ee18caa
1191111 <1191111@isep.ipp.pt>
2021-05-30 12:42:23 +0100

    LAPR4-43 - Implementation -correção codigo de erro, tratamento de exceções

--- Projeto/eapli.base/base.app.workflow.engine/src/main/java/eapli/base/workflow/engine/TcpServer.java
+++ Projeto/eapli.base/base.app.workflow.engine/src/main/java/eapli/base/workflow/engine/TcpServer.java
@@ -4,7 +4,6 @@ package eapli.base.workflow.engine;
 import eapli.base.Application;
 import eapli.base.collaborator.domain.Collaborator;
 import eapli.base.collaborator.domain.InstituionalEmail;
-import eapli.base.collaborator.domain.MecanographicNumber;
 import eapli.base.collaborator.dto.CollaboratorDTO;
 import eapli.base.collaborator.repositories.CollaboratorRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
@@ -30,134 +29,142 @@ import java.util.Optional;
 
 public class TcpServer implements Runnable {
 
-	private static ServerSocket serverSocket;
-	private Socket clientSocket;
-	private DataOutputStream sOut;
-	private DataInputStream sIn;
+    private static ServerSocket serverSocket;
+    private Socket clientSocket;
+    private DataOutputStream sOut;
+    private DataInputStream sIn;
 
-	public TcpServer(Socket cli_s) {
-		clientSocket = cli_s;
-	}
+    public TcpServer(Socket cli_s) {
+        clientSocket = cli_s;
+    }
 
-	public void stopConnection(InetAddress clientIP) {
-		byte[] serverResponse= {(byte)0, (byte)2, (byte)0, (byte)0};
-		try {
-			sOut.write(serverResponse);
-			sOut.flush();
-			System.out.println("Client " + clientIP.getHostAddress() + ", port number: " + clientSocket.getPort() + " disconnected");
-			clientSocket.close();
-		}catch (IOException ex){
-			System.out.println("Falied to close client socket");
-		}
-		}
+    public void stopConnection(InetAddress clientIP) {
+        byte[] serverResponse = {(byte) 0, (byte) 2, (byte) 0, (byte) 0};
+        try {
+            sOut.write(serverResponse);
+            sOut.flush();
+            System.out.println("Client " + clientIP.getHostAddress() + ", port number: " + clientSocket.getPort() + " disconnected");
+            clientSocket.close();
+        } catch (IOException ex) {
+            System.out.println("Falied to close client socket");
+        }
+    }
 
-	public void TaskList() {
-		try {
-			//Sends response to the client
-			byte[] serverResponse = {(byte) 0, (byte) 2, (byte) 0, (byte) 0};
-			sOut.write(serverResponse);
-			sOut.flush();
+    public void TaskList() {
+        try {
+            //Sends response to the client
+            byte[] serverResponse = {(byte) 0, (byte) 2, (byte) 0, (byte) 0};
+            sOut.write(serverResponse);
+            sOut.flush();
 
-			//Recives email
-			byte[] emailInfo = sIn.readNBytes(3);
-			byte[] emailByteArray = sIn.readNBytes(emailInfo[2]);
-			String email = new String(emailByteArray, StandardCharsets.UTF_8);
+            //Receives email
+            byte[] emailInfo = sIn.readNBytes(3);
+            byte[] emailByteArray = sIn.readNBytes(emailInfo[2]);
+            String email = new String(emailByteArray, StandardCharsets.UTF_8);
 
-			//Get Collaborator by email
-			CollaboratorRepository collaboratorRepository = PersistenceContext.repositories().collaborators();
-			InstituionalEmail iEmail = new InstituionalEmail(email);
-			Optional<Collaborator> collabOp =collaboratorRepository.getColaboradorByEmail(iEmail);
-			CollaboratorDTO collaboratorDTO;
-			if (collabOp.isPresent())
-				collaboratorDTO= collabOp.get().toDTO();
+            //Get Collaborator by email
+            CollaboratorRepository collaboratorRepository = PersistenceContext.repositories().collaborators();
+            InstituionalEmail iEmail = new InstituionalEmail(email);
+            Optional<Collaborator> collabOp = collaboratorRepository.getColaboradorByEmail(iEmail);
+            CollaboratorDTO collaboratorDTO;
+            if (collabOp.isPresent())
+                collaboratorDTO = collabOp.get().toDTO();
+            else {
+                System.out.println("Invalid email");
+                System.exit(1);
+            }
 
-			//Get Ticket by collaborator ID
-			List<TicketTask>lstTicketTask = new ArrayList<>();
+            //Get Ticket by collaborator TODO: Por implementar
+            List<TicketTask> lstTicketTask = new ArrayList<>();
 
-			TicketRepository ticketRepository = PersistenceContext.repositories().tickets();
-			int finalCode=254;
-			for (TicketTask ticketTask:lstTicketTask){
-				Optional<Ticket> ticketOp = ticketRepository.ofIdentity(new TicketID(ticketTask.identity().toString()));
-				Ticket ticket;
-				if (ticketOp.isPresent())
-					ticket=ticketOp.get();
-				else{
-					System.err.println("No tickets associated with this ticketTast");
-					finalCode=253;
-					break;
-				}
-				TicketDTO ticketDTO =ticket.toDTO();
-				ServiceDTO serviceDTO = ticket.service().toDTO();
-				sendString(ticketDTO.urgency); //urgency
-				sendString(ticketDTO.deadLine); //deadline
-				sendString(serviceDTO.title); //title
-				sendString(serviceDTO.icon); //icon
-				sendString(serviceDTO.briefDescription); //briefDesc
-				sendString(ticket.service().catalogo().toDTO().nivelCriticidade.toString()); //criticityLvl
-			}
-			byte[] finalPackage ={(byte) 0, (byte) 254, (byte) 0, (byte) 0};
-			sOut.write(finalPackage);
+            TicketRepository ticketRepository = PersistenceContext.repositories().tickets();
+            int finalCode = 254;
+            for (TicketTask ticketTask : lstTicketTask) {
+                Optional<Ticket> ticketOp = ticketRepository.ofIdentity(new TicketID(ticketTask.identity().toString()));
+                Ticket ticket;
+                if (ticketOp.isPresent())
+                    ticket = ticketOp.get();
+                else {
+                    System.err.println("No tickets associated with this ticketTast");
+                    finalCode = 253;
+                    break;
+                }
+                TicketDTO ticketDTO = ticket.toDTO();
+                ServiceDTO serviceDTO = ticket.service().toDTO();
+                sendString(ticketDTO.urgency); //urgency
+                sendString(ticketDTO.deadLine); //deadline
+                sendString(serviceDTO.title); //title
+                sendString(serviceDTO.icon); //icon
+                sendString(serviceDTO.briefDescription); //briefDesc
+                sendString(ticket.service().catalogo().toDTO().nivelCriticidade.toString()); //criticityLvl
+            }
+            byte[] finalPackage = {(byte) 0, (byte) finalCode, (byte) 0, (byte) 0};
+            sOut.write(finalPackage);
+            sOut.flush();
+        } catch (IOException ex) {
+            System.out.println("An error ocurred");
+        }
+    }
 
-		} catch (IOException ex) {
-			System.out.println("An error ocurred");
-		}
-	}
-
-	public void sendString(String s) throws IOException {
-		byte[] byteArray=s.getBytes(StandardCharsets.UTF_8);
-		byte[] info ={(byte) 0, (byte) 255, (byte) byteArray.length};
-		byte[] fullPackage = ArrayUtils.addAll(info,byteArray);
-		sOut.write(fullPackage);
-		sOut.flush();
-	}
+    public void sendString(String s) throws IOException {
+        byte[] byteArray = s.getBytes(StandardCharsets.UTF_8);
+        byte[] info = {(byte) 0, (byte) 255, (byte) byteArray.length};
+        byte[] fullPackage = ArrayUtils.addAll(info, byteArray);
+        sOut.write(fullPackage);
+        sOut.flush();
+    }
 
 
-	public void run () {
-		InetAddress clientIP=clientSocket.getInetAddress();
-		System.out.println( "New client connection from " + clientIP.getHostAddress( ) + ", port number " + clientSocket.getPort( ) );
+    public void run() {
+        InetAddress clientIP = clientSocket.getInetAddress();
+        System.out.println("New client connection from " + clientIP.getHostAddress() + ", port number " + clientSocket.getPort());
 
-		try {
-			sOut = new DataOutputStream( clientSocket.getOutputStream( ));
-			sIn = new DataInputStream( clientSocket.getInputStream( ));
-			boolean cycle = true;
+        try {
+            sOut = new DataOutputStream(clientSocket.getOutputStream());
+            sIn = new DataInputStream(clientSocket.getInputStream());
+        } catch (IOException e) {
+            System.out.println("Failed to establish DataOutputStream or DataInputStream");
+            System.exit(1);
+        }
 
-			while (cycle){
-				byte[] clientRequest = sIn.readNBytes(4);
-				switch (clientRequest[1]){
-					case 1:
-						stopConnection(clientIP);
-						cycle=false;
-						break;
-					case 3:
-						TaskList();
-						break;
-				}
+        try {
+            boolean cycle = true;
+            while (cycle) {
+                byte[] clientRequest = sIn.readNBytes(4);
+                switch (clientRequest[1]) {
+                    case 1:
+                        stopConnection(clientIP);
+                        cycle = false;
+                        break;
+                    case 3:
+                        TaskList();
+                        break;
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("An error ocurred");
+        }
+    }
 
-			}
-		} catch ( IOException ex ) {
-			System.out.println( "An error ocurred" );
-		}
-	}
+    public static void main(String[] args) {
+        Socket cliSock;
+        AuthzRegistry.configure(PersistenceContext.repositories().users(), new BasePasswordPolicy(), new PlainTextEncoder());
+        try {
+            serverSocket = new ServerSocket(Integer.parseInt(Application.settings().getPortWorkflow()));
+        } catch (IOException ex) {
+            System.err.println("Failed to open server socket");
+            System.exit(1);
+        }
+        while (true) {
+            try {
+                cliSock = serverSocket.accept();
+                new Thread(new TcpServer(cliSock)).start();
 
-	public static void main(String[] args) {
-		Socket cliSock;
-		AuthzRegistry.configure(PersistenceContext.repositories().users(), new BasePasswordPolicy(), new PlainTextEncoder());
-		try {
-			serverSocket = new ServerSocket(Integer.parseInt(Application.settings().getPortWorkflow()));
-		} catch (IOException ex) {
-			System.err.println("Failed to open server socket");
-			System.exit(1);
-		}
-		while (true) {
-			try {
-				cliSock = serverSocket.accept();
-				new Thread(new TcpServer(cliSock)).start();
-
-			} catch (IOException e) {
-				System.out.println("failed to accept client socket");
-			}
-		}
-	}
+            } catch (IOException e) {
+                System.out.println("failed to accept client socket");
+            }
+        }
+    }
 }
 
 
--- Projeto/eapli.base/base.app.workflow.engine/src/main/java/eapli/base/workflow/engine/client/TcpClient.java
+++ Projeto/eapli.base/base.app.workflow.engine/src/main/java/eapli/base/workflow/engine/client/TcpClient.java
@@ -109,6 +109,11 @@ public class TcpClient {
 			count++;
 		}
 
+		if (taskInfoList.isEmpty()){
+			System.out.println("Não existem tarefas associadas a este colaborador");
+			return null;
+		}
+
 		return taskInfoList;
 
 	}
