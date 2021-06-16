0223646f63565ee4c2454f0792d929b70b7da887
1190731 <1190731@isep.ipp.pt>
2021-06-16 23:23:56 +0100

    LAPR4-84 implementation

--- Projeto/eapli.base/base.core/src/main/java/eapli/base/ticket/application/TicketService.java
+++ Projeto/eapli.base/base.core/src/main/java/eapli/base/ticket/application/TicketService.java
@@ -1,7 +1,13 @@
 package eapli.base.ticket.application;
 
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.SftpException;
 import eapli.base.Application;
 import eapli.base.dasboard.application.TcpClient;
+import eapli.base.form.domain.Form;
+import eapli.base.utils.SFTPClient;
+
+import java.io.File;
 
 public class TicketService {
 
@@ -15,6 +21,17 @@ public class TicketService {
 		tcpClient.stopConnection();
 
 		return sentSuccessfully;
+	}
+
+	public File getFIleFromServer(Form form){
+		String script = form.script().toString();
+		SFTPClient sftp = new SFTPClient();
+		try {
+			return sftp.getScript(script);
+		} catch (JSchException | SftpException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
