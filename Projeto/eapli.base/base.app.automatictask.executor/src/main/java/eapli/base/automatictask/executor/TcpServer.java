229e5ad690be9271578d85d9e4e754deac5d2ea0
1190731 <1190731@isep.ipp.pt>
2021-06-19 21:14:47 +0100

    LAPR4-75 Fix null pointer

--- Projeto/eapli.base/base.app.automatictask.executor/src/main/java/eapli/base/automatictask/executor/TcpServer.java
+++ Projeto/eapli.base/base.app.automatictask.executor/src/main/java/eapli/base/automatictask/executor/TcpServer.java
@@ -141,12 +141,12 @@ class TcpServerThread implements Runnable {
             List<String> answerData = new ArrayList<>();
             List<String> approvalData = new ArrayList<>();
 
-            if(data.length>2) {
+            if (data.length > 2) {
                 String answerString = data[2];
                 answerData = Arrays.asList(answerString.split(";;;"));
             }
 
-            if(data.length>3) {
+            if (data.length > 3) {
                 String approvalString = data[3];
                 approvalData = Arrays.asList(approvalString.split(";;;"));
             }
@@ -158,24 +158,27 @@ class TcpServerThread implements Runnable {
             Pair<Boolean, String> wasItSuccessfull = null;
             Pair<Boolean, String> wasItSuccessfull2 = null;
 
-            if (Application.settings().getGRAMMARAUTOMATICTYPE().equals("VISITOR")){
-               wasItSuccessfull = ExecutorAtividadeAutomatica.parseWithVisitor(email, script.getName(), answerData, approvalData);
-            }else{
+            if (Application.settings().getGRAMMARAUTOMATICTYPE().equals("VISITOR")) {
+                wasItSuccessfull = ExecutorAtividadeAutomatica.parseWithVisitor(email, script.getName(), answerData, approvalData);
+            } else {
                 wasItSuccessfull2 = ExecutorAtividadeAutomatica.parseWithListener(email, script.getName(), answerData, approvalData);
             }
 
             calendar = Calendar.getInstance();
-            if (wasItSuccessfull.a && Application.settings().getGRAMMARAUTOMATICTYPE().equals("VISITOR"))
-                System.out.printf("[%s] - %s executed successfully.%n", calendar.getTime(), scriptName);
-            else
-                System.out.printf("[%s] - Something went wrong when executing %s: %s%n", calendar.getTime(), scriptName, wasItSuccessfull.b);
+            if (Application.settings().getGRAMMARAUTOMATICTYPE().equals("VISITOR")) {
+                if (wasItSuccessfull.a)
+                    System.out.printf("[%s] - %s executed successfully.%n", calendar.getTime(), scriptName);
+                else
+                    System.out.printf("[%s] - Something went wrong when executing %s: %s%n", calendar.getTime(), scriptName, wasItSuccessfull.b);
+            }
 
-            if (wasItSuccessfull2.a && !Application.settings().getGRAMMARAUTOMATICTYPE().equals("VISITOR"))
-                System.out.printf("[%s] - %s executed successfully.%n", calendar.getTime(), scriptName);
-            else
-                System.out.printf("[%s] - Something went wrong when executing %s: %s%n", calendar.getTime(), scriptName, wasItSuccessfull2.b);
-
-            return wasItSuccessfull.a ;
+            if (!Application.settings().getGRAMMARAUTOMATICTYPE().equals("VISITOR")) {
+                if (wasItSuccessfull2.a)
+                    System.out.printf("[%s] - %s executed successfully.%n", calendar.getTime(), scriptName);
+                else
+                    System.out.printf("[%s] - Something went wrong when executing %s: %s%n", calendar.getTime(), scriptName, wasItSuccessfull2.b);
+            }
+            return wasItSuccessfull.a;
         } catch (IOException | JSchException | SftpException ex) {
             System.out.println("An error ocurred");
             ex.printStackTrace();
