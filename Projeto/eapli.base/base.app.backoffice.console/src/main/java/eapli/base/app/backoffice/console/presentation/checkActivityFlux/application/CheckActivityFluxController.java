48364d26a106837c234edfc24744204cac402a05
1190731 <1190731@isep.ipp.pt>
2021-06-20 15:29:08 +0100

    LAPR4-81 adicionado suporte para varios servidores.

--- Projeto/eapli.base/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/checkActivityFlux/application/CheckActivityFluxController.java
+++ Projeto/eapli.base/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/checkActivityFlux/application/CheckActivityFluxController.java
@@ -6,7 +6,13 @@ import eapli.base.app.backoffice.console.presentation.checkActivityFlux.HttpServ
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 public class CheckActivityFluxController {
     private final AuthorizationService authz = AuthzRegistry.authorizationService();
@@ -17,16 +23,27 @@ public class CheckActivityFluxController {
         server.start();
     }
 
-    public void setStatus(){
-        server.setStatus(checkServer(Application.settings().getWorkflowServer()));
+    public void setStatus() {
+        ArrayList<String> servidores = new ArrayList<>();
+        try {
+            File myObj = new File("Executer_ip_list.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String ip = myReader.nextLine();
+                servidores.add(checkServer(ip.trim()));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        server.setStatusList(servidores);
     }
 
-    public String checkServer ( String ipAddress ) {
+        public String checkServer ( String ipAddress ) {
         try {
             InetAddress inet = InetAddress.getByName( ipAddress );
 
             if ( inet.isReachable( 1500 ) ) {
-                return ( "O servidor responsável pelo motor de fluxo de atividades está online." );
+                return ( "O servidor responsavel pelo motor de fluxo de atividades esta online." );
             } else {
                 return(  "O servidor responsável pelo motor de fluxo de atividades encontra-se offline." );
             }
